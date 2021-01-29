688772a8a55eda16396d6119e84fd3f6fe986622
sameryqawasmeh <sameryqawasmeh@gmail.com>
2021-01-28 17:01:44 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello Dojo</h1>
+    <p>Things i need to do</p>
+    <ul>
+      <li>learn react</li>
+      <li>climb mt. everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ul>
+    </div>
+  );
+}
+
+export default App;
